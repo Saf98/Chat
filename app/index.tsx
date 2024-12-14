98dfb9b05c3ef2c31f2a7ae418c645adb98d3ea6
Saf98 <sbilimoria98@gmail.com>
2024-12-14 22:00:16 +0000

    init

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,68 @@
-import { Text, View } from "react-native";
+import {
+	KeyboardAvoidingView,
+	Text,
+	TextInput,
+	View,
+	Button,
+	ActivityIndicator,
+} from "react-native";
+import React, { useState } from "react";
 import { Link } from "expo-router";
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import * as z from "zod";
-
 export default function Index() {
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [loading, setLoading] = useState<Boolean>(false);
 	type Inputs = {
 		example: string;
 		exampleRequired: string;
 	};
 
-	const Login = () => {
-		const {
-			register,
-			handleSubmit,
-			watch,
-			formState: { errors },
-		} = useForm<Inputs>();
-		const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+	const registerEmail = () => {
+		return;
+	};
+	const registerPassword = () => {
+		return;
+	};
+	const signUp = async () => {
+		// setLoading(true);
+		// try {
+		// 	const user = await auth().createUserWithEmailAndPassword(email, password);
+		// } catch (e: any) {
+		// 	const error = e as FirebaseError;
+		// 	alert(error?.message);
+		// } finally {
+		// 	setLoading(false);
+		// }
+		return;
+	};
 
-		console.log(watch("example")); // watch input value by passing the name of it
-
-		return (
-			/* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-			<form onSubmit={handleSubmit(onSubmit)}>
-				{/* register your input into the hook by invoking the "register" function */}
-				<input defaultValue="test" {...register("example")} />
-
-				{/* include validation with required or other standard HTML validation rules */}
-				<input {...register("exampleRequired", { required: true })} />
-				{/* errors will return when field validation fails  */}
-				{errors.exampleRequired && <span>This field is required</span>}
-
-				<input type="submit" />
-			</form>
-		);
+	const signIn = async () => {
+		// setLoading(true);
+		// try {
+		// 	const user = await auth().createUserWithEmailAndPassword(email, password);
+		// } catch (e: any) {
+		// 	const error = e as FirebaseError;
+		// 	alert(error?.message);
+		// } finally {
+		// 	setLoading(false);
+		// }
+		return;
 	};
 
 	return (
 		<View>
-			<Login />
-			<Link href="/register">Go to Register screen</Link>
+			<KeyboardAvoidingView behavior="padding">
+				<Text>Enter Your Email</Text>
+				<TextInput value="" onChangeText={registerEmail} />
+				<Text>Enter Your Password</Text>
+				<TextInput value="" onChangeText={registerPassword} />
+				<Link href="/register">Go to Register screen</Link>
+				{loading ? (
+					<ActivityIndicator />
+				) : (
+					<Button onPress={signUp} title="Sign Up" />
+				)}
+			</KeyboardAvoidingView>
 		</View>
 	);
 }
--- app/register.tsx
+++ app/register.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function AboutScreen() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.text}>About screen</Text>
+			<Link href="./">Home</Link>
 		</View>
 	);
 }
